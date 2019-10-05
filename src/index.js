fa025c83c0422c988d9fb4c647c81a0145bad4d3
Alex89889 <mihal4encko.sasha@yandex.ru>
2019-10-05 12:39:01 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ var twoBrackets;
+ for (var i = str.length; i >= 0; i--){ 
+    for (var j = 0; j < bracketsConfig.length; j++){
+	   twoBrackets= bracketsConfig[j][0]+bracketsConfig[j][1];
+       str = str.replace(twoBrackets, '');
+    }
+  }
+  return (str.length == 0);
 }
